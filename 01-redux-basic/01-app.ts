e09ae60513d3f652c5a052364f5f32c47206d419
Oscar Munoz Bernales <munoz.bernales.oscar@gmail.com>
2022-10-28 15:59:25 -0300

    UPD: terminando seccion 1

--- 01-redux-basic/01-app.ts
+++ 01-redux-basic/01-app.ts
@@ -7,14 +7,44 @@ interface Action {
 
 
 function reducer(state = 10, action: Action) {
-    if (action.type == 'INCREMENTAR') {
-        state += 1;
+    switch(action.type){
+        case 'INCREMENTAR':
+            return state += 1;
+        case 'DECREMENTAR':
+            return state -= 1;
+        case 'MULTIPLICAR':
+            return state *= action.payload;
+        case 'DIVIDIR':
+            return state /= action.payload;
+        default:
+            return state;
     }
-    return state;
 }
 const incrementadorAction: Action = {
     type: 'INCREMENTAR'
 }
 
+const decrementarAction: Action = {
+    type: 'DECREMENTAR'
+}
+
+const multiplicarAction: Action = {
+    type: 'MULTIPLICAR',
+    payload: 2
+}
+const dividirAction: Action = {
+    type: 'DIVIDIR',
+    payload: 10
+}
+
 const result = reducer(100, incrementadorAction);
 console.log("🚀 ~ file: 01-app.ts ~ line 20 ~ result", result);
+
+const result1 = reducer(100, decrementarAction);
+console.log("🚀 ~ file: 01-app.ts ~ line 30 ~ result1", result1);
+
+const result2 = reducer(100, multiplicarAction);
+console.log("🚀 ~ file: 01-app.ts ~ line 40 ~ result2", result2);
+
+const result3 = reducer(100, dividirAction);
+console.log("🚀 ~ file: 01-app.ts ~ line 50 ~ result3", result3);
